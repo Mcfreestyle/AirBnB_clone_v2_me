0f317496c04f5811268e2a266cf7929fa5df8b9e
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-05-13 15:10:12 -0500

    chore: definition of Place model after to conditional

--- models/place.py
+++ models/place.py
@@ -7,20 +7,44 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Table, Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 
-storage = getenv('HBNB_TYPE_STORAGE')
-
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60),
-                             ForeignKey('places.id'),
-                             primary_key=True),
-                      Column('amenity_id', String(60),
-                             ForeignKey('amenities.id'),
-                             primary_key=True)
-                      )
+# if getenv('HBNB_TYPE_STORAGE') == 'db':
+place_amenity = Table('place_amenity',
+                      Base.metadata,
+                      Column('place_id',
+                             String(60),
+                             ForeignKey('places.id')),
+                      Column('amenity_id',
+                             String(60),
+                             ForeignKey('amenities.id')))
 
 
 class Place(BaseModel, Base):
     '''Place Class'''
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        amenity_ids = []
+
+        @property
+        def reviews(self):
+            reviews = models.storage.all('Review')
+            list_review = []
+            for review in reviews.values():
+                if review.place_id == self.id:
+                    list_review.append(review)
+            return (list_review)
+
+        @property
+        def amenities(self, ameni=None):
+            if obj.__class__.__name__ == 'Amenity':
+                self.amenity_ids.append(ameni.id)
+            if self.amenity_ids.count(ameni.id) == 1:
+                dict_obj = models.storage.all("Amenity")
+                for obj in dict_obj.values():
+                    if ameni.id == obj.id:
+                        self.amenity_ids.pop()
+                        break
+            return (self.amenity_ids)
+
     __tablename__ = 'places'
 
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
@@ -33,34 +57,10 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer(), nullable=False, default=0)
     latitude = Column(Float(), nullable=True, default=0.000)
     longitude = Column(Float(), nullable=True, default=0.000)
-    amenity_ids = []
 
-    amenities = relationship('Amenity',
-                             secondary=place_amenity,
-                             viewonly=False)
+    # if getenv('HBNB_TYPE_STORAGE') == 'db':
     reviews = relationship('Review', backref='place')
-
-    if storage != 'db':
-        @property
-        def reviews(self):
-            reviews = models.storage.all('Review')
-            list_review = []
-            for review in reviews.values():
-                if review.place_id == self.id:
-                    list_review.append(review)
-            return (list_review)
-
-        @property
-        def amenities(self):
-            return (self.amenity_ids)
-
-        @amenities.setter
-        def amenities(self, ameni=None):
-            if obj.__class__.__name__ == 'Amenity':
-                self.amenity_ids.append(ameni.id)
-            if self.amenity_ids.count(ameni.id) == 1:
-                dict_obj = models.storage.all("Amenity")
-                for obj in dict_obj.values():
-                    if ameni.id == obj.id:
-                        self.amenity_ids.pop()
-                        break
+    amenities = relationship('Amenity',
+                             secondary='place_amenity',
+                             back_populates='place_amenities',
+                             viewonly=False)
